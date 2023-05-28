17bbb21ec26929e760ba8f4513be35ecbfed477b
Ben Smerd <bsmerd@adam.com.au>
2023-05-28 20:56:21 +0930

    add in the route path /deal for deal routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const businessRoutes = require("./businessRoutes.js");
+const dealRoutes = require("./dealRoutes.js");
 
 router.use("/businesses", businessRoutes);
+router.use("/deals", dealRoutes);
 
 module.exports = router;
