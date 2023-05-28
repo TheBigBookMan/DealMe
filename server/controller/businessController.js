fa51166f1a66c8de821fd0068cb0e69d780b6936
Ben Smerd <bsmerd@adam.com.au>
2023-05-28 20:02:44 +0930

    added in the getSingleBusiness GET functionality

--- server/controller/businessController.js
+++ server/controller/businessController.js
@@ -25,6 +25,20 @@ const businessHttp = {
             res.status(500).json({ message: "Server Error" });
         }
     },
+    getSingleBusiness: async (req, res) => {
+        try {
+            const { businessId } = req.params;
+            const business = await prisma.businesses.findUnique({
+                where: {
+                    id: businessId,
+                },
+            });
+            res.json(business);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: "Couldn't find business." });
+        }
+    },
 };
 
 module.exports = businessHttp;
