c0d4a827e20c0b89ac5ad7c7813a8d5832014287
Ben Smerd <bsmerd@adam.com.au>
2023-05-28 20:03:17 +0930

    added in the route for businessId with the GET function call

--- server/routes/api/businessRoutes.js
+++ server/routes/api/businessRoutes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const businessController = require("../../controller/businessController.js");
 
+// ? Route '/'
 router
     .route("/")
     .get(businessController.getAllBusiness)
     .post(businessController.createBusiness);
+
+// ? Route '/:businessId'
+router.route("/:businessId").get(businessController.getSingleBusiness);
 
 module.exports = router;
