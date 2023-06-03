d5048f05428ec53abbec446165c7d352d1baf60a
Ben Smerd <bsmerd@adam.com.au>
2023-06-03 20:13:53 +0930

    add in the route for Deals with the deals/:dealId

--- server/routes/api/dealRoutes.js
+++ server/routes/api/dealRoutes.js
@@ -4,4 +4,7 @@ const dealController = require("../../controller/dealController");
 // ? Route '/'
 router.route("/").get(dealController.getAllDeals).post(dealController.addDeal);
 
+// ? Route '/:dealId'
+router.route("/:dealId").get(dealController.getDeal);
+
 module.exports = router;
