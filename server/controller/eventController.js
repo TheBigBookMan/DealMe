d2594f9de297f678982b8e038f2dcb07c58768e1
Ben Smerd <bsmerd@adam.com.au>
2023-06-05 20:01:10 +0930

    added in the getEvent controller for the event

--- server/controller/eventController.js
+++ server/controller/eventController.js
@@ -12,6 +12,22 @@ const eventHttp = {
             });
         }
     },
+    getEvent: async (req, res) => {
+        try {
+            const { eventId } = req.params;
+            const event = await prisma.event.findUnique({
+                where: {
+                    id: eventId,
+                },
+            });
+            res.json(event);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: "Could not get event, please try again.",
+            });
+        }
+    },
 };
 
 module.exports = eventHttp;
