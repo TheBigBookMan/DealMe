230e1f499a11f3ad4de584c12a0629c8c80cfb79
Ben Smerd <bsmerd@adam.com.au>
2023-06-05 20:08:52 +0930

    added in the post request for the base events path

--- server/routes/api/eventRoutes.js
+++ server/routes/api/eventRoutes.js
@@ -2,7 +2,10 @@ const router = require("express").Router();
 const eventController = require("../../controller/eventController");
 
 // ? Route '/'
-router.route("/").get(eventController.getAllEvents);
+router
+    .route("/")
+    .get(eventController.getAllEvents)
+    .post(eventController.addEvent);
 
 // ? Route '/:eventId'
 router.route("/:eventId").get(eventController.getEvent);
