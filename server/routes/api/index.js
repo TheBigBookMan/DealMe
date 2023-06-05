7251df3bf08319c6f49f6686895b670c8c69ff5b
Ben Smerd <bsmerd@adam.com.au>
2023-06-05 19:49:12 +0930

    added in the events route base path /events

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const businessRoutes = require("./businessRoutes.js");
 const dealRoutes = require("./dealRoutes.js");
+const eventRoutes = require("./eventRoutes.js");
 
 router.use("/businesses", businessRoutes);
 router.use("/deals", dealRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
