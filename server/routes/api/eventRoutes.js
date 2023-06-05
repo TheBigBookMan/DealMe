2becfe4406d3c40c5050adbd730f3e29b8ac671f
Ben Smerd <bsmerd@adam.com.au>
2023-06-05 20:00:53 +0930

    added in the get request for the events/:eventId path

--- server/routes/api/eventRoutes.js
+++ server/routes/api/eventRoutes.js
@@ -4,4 +4,7 @@ const eventController = require("../../controller/eventController");
 // ? Route '/'
 router.route("/").get(eventController.getAllEvents);
 
+// ? Route '/:eventId'
+router.route("/:eventId").get(eventController.getEvent);
+
 module.exports = router;
