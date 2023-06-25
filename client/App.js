09c07f958129271429e4d5bddacadc314242f8f8
Ben Smerd <bsmerd@adam.com.au>
2023-06-25 19:38:53 +0930

    installed react native and nativewindcss for client side

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View>
+            <Text className="font-bold bg-blue-400">OHey there baby!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
