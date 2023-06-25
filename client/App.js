48dbb8694be90062a19024b244c8ecd4de8eb050
Ben Smerd <bsmerd@adam.com.au>
2023-06-25 20:06:08 +0930

    install expo icons

--- client/App.js
+++ client/App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, SafeAreaView, TextInput } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useState } from "react";
 
 export default function App() {
+    const [colorIcon, setColorIcon] = useState("green");
+
+    const changeColor = (color) => {
+        if (color === "green") {
+            setColorIcon("blue");
+        } else {
+            setColorIcon("green");
+        }
+    };
+
     return (
-        <View>
-            <Text className="font-bold bg-blue-400">OHey there baby!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <SafeAreaView>
+            <View className="h-full flex justify-center items-center">
+                <Text className="font-bold bg-blue-400">OHey there baby!</Text>
+                <Text className="text-green-300 font-bold">LETSSS GOOOD</Text>
+                <Ionicons
+                    name="md-checkmark-circle"
+                    size={100}
+                    color={colorIcon}
+                    onPress={() => changeColor(colorIcon)}
+                />
+                <StatusBar style="auto" />
+            </View>
+        </SafeAreaView>
     );
 }
