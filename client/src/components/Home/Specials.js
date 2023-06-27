62d8d739ac740d917721c53e4c0f7bc7d7a878b5
Ben Smerd <bsmerd@adam.com.au>
2023-06-27 20:47:35 +0930

    add in new files

---
+++ client/src/components/Home/Specials.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+// ? Maybe a "special offers" section where companies can pay us to promote even more on the homepage
+// ? What's on tonight section?????
+
+const Specials = () => {
+    return (
+        <View>
+            <Text>Specials</Text>
+        </View>
+    );
+};
+
+export default Specials;
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,19 +1,20 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, ScrollView } from "react-native";
+import SearchBar from "../components/Home/SearchBar";
+import Categories from "./../components/Home/Categories";
+import Maps from "./../components/Home/Maps";
+import Specials from "./../components/Home/Specials";
 
 // ? Can have multple components, maybe a search bar at top
-// ? A map with nearby locations with maybe little red dots on them on the map close by where something is on and maybe yellow dots fr favourites places that have things on for that night
-// ? Maybe a "special offers" section where companies can pay us to promote even more on the homepage
-// ? What's on tonight section?????
 
 const Home = () => {
     return (
         <SafeAreaView>
-            <View>
-                <Image
-                    className="w-full h-16"
-                    source={require("../images/logo.png")}
-                />
-            </View>
+            <ScrollView className="w-full h-full">
+                <SearchBar />
+                <Categories />
+                <Maps />
+                <Specials />
+            </ScrollView>
         </SafeAreaView>
     );
 };
