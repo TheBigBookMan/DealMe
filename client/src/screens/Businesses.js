d9df847928aaa42f538b49a3af35b819920ddaba
Ben Smerd <bsmerd@adam.com.au>
2023-08-28 19:02:24 +0930

    fix businesses bug

--- client/src/screens/Businesses.js
+++ client/src/screens/Businesses.js
@@ -1,12 +1,28 @@
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, StatusBar, FlatList } from "react-native";
+import BusinessItem from "../components/Businesses/BusinessItem";
+import { businesses } from "../utils/businesses";
 
 // ? This will just list all the businesses
 
 const Businesses = () => {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>Businesses</Text>
+        <SafeAreaView
+            nestedScrollEnabled={true}
+            className="bg-bg-dark text-slate-300"
+        >
+            <StatusBar barStyle="light-content" />
+            <View className="w-full h-full bg-bg-dark text-white p-4">
+                <Text className="text-red-500 text-2xl font-bold">
+                    Businesses
+                </Text>
+                <Text className="text-slate-400">
+                    Select a Business to view more...
+                </Text>
+                <FlatList
+                    className="w-full h-full overflow-y-auto"
+                    data={businesses}
+                    renderItem={({ item }) => <BusinessItem data={item} />}
+                ></FlatList>
             </View>
         </SafeAreaView>
     );
