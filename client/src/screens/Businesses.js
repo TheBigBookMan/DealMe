b5c2cbb50a94e5a05f419ad75a3d1ffe6877fddb
Ben Smerd <bsmerd@adam.com.au>
2023-08-30 12:55:44 +0930

    add in dropdown select menu to the businesses page

--- client/src/screens/Businesses.js
+++ client/src/screens/Businesses.js
@@ -2,6 +2,16 @@ import { View, Text } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { useState } from "react";
 import BusinessList from "../components/Businesses/BusinessList";
+import SelectDropdown from "react-native-select-dropdown";
+
+const businessCategories = [
+    "Bar",
+    "Pub",
+    "Restaurant",
+    "NightClub",
+    "EventVenue",
+    "Festival",
+];
 
 const Businesses = () => {
     const [searchQuery, setSearchQuery] = useState("");
@@ -12,8 +22,15 @@ const Businesses = () => {
         alert(searchQuery);
     };
 
+    // TODO fix this up to filter the venue type
+    const selectType = (e) => {
+        console.log(e);
+        alert(e);
+    };
+
+    // TODO the color of the text is not adjustable??? need to look closer, the inline css-js 'color' property not working
     return (
-        <View>
+        <View className="flex flex-col gap-2 p-1 px-2">
             <Text className="font-bold text-red-500 p-2 text-3xl">
                 Businesses
             </Text>
@@ -24,6 +41,24 @@ const Businesses = () => {
                 placeholder="Search"
                 onChangeText={onChangeSearch}
                 value={searchQuery}
+            />
+            <SelectDropdown
+                data={businessCategories}
+                onSelect={(e) => selectType(e)}
+                defaultButtonText={"Type"}
+                dropdownStyle={{
+                    borderBottomEndRadius: "20px",
+                    backgroundColor: "black",
+                    borderColor: "red",
+                }}
+                buttonStyle={{
+                    borderRadius: "20px",
+                    backgroundColor: "black",
+                    borderWidth: "2px",
+                    color: "red",
+                    borderColor: "red",
+                }}
+                selectedRowStyle={{ backgroundColor: "tomato" }}
             />
             <BusinessList />
         </View>
