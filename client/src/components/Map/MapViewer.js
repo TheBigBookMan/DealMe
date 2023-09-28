07f35327feb4d43bbfc7f2d19c1cd84e4aa54bd8
Ben Smerd <bsmerd@adam.com.au>
2023-09-28 12:38:31 +0930

    add in the MarkerMaker with coordinate data to create a marker on the google maps

--- client/src/components/Map/MapViewer.js
+++ client/src/components/Map/MapViewer.js
@@ -1,33 +1,61 @@
-import MapView, { Marker } from "react-native-maps";
+import MapView from "react-native-maps";
+import { useEffect, useState } from "react";
+import { api } from "../../utils/api";
+import { Text } from "react-native";
+import MarkerMaker from "./MarkerMaker";
 
 // TODO need to make it so the lattitude and longitude initialRegion for the MapViewer is the users phone geolocations
 // TODO could probably have the phone indicator move with the user as they move becase they would be walking to the location???
 
+// TODO MapViewer initialregion can be the state of the location of the user, ORRR maybe we dont need a constantly updating map for the MVP because we can just have the initial region be gathered from the current location and then other screens can have their filters automatically set to a area based on the coordinates as well
+
 const MapViewer = () => {
-    return (
-        <MapView
-            style={{ width: "100%", height: "100%" }}
-            provider="google"
-            initialRegion={{
-                latitude: -34.9236189,
-                longitude: 138.6313699,
-                latitudeDelta: 0.0422,
-                longitudeDelta: 0.0421,
-            }}
-            showsUserLocation={true}
-            showsCompass={true}
-            showsScale={true}
-        >
-            <Marker
-                coordinate={{
+    const [businessData, setBusinessData] = useState();
+
+    const getAllBusinesses = async () => {
+        try {
+            const response = await api.get("/businesses");
+            if (response.status === 200) {
+                // !!!! testing with just colonist for dev
+                const colonise = response.data.filter(
+                    (bus) => bus.name === "The Colonist"
+                );
+                // console.log(colonise);
+                // setBusinessData(response.data);
+                setBusinessData(colonise);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getAllBusinesses();
+    }, []);
+
+    if (!businessData) {
+        return <Text>Loading...</Text>;
+    } else {
+        return (
+            <MapView
+                style={{ width: "100%", height: "100%" }}
+                provider="google"
+                initialRegion={{
                     latitude: -34.9236189,
                     longitude: 138.6313699,
+                    latitudeDelta: 0.0422,
+                    longitudeDelta: 0.0421,
                 }}
-                title="Current user"
-                description="Where you currently are"
-            />
-        </MapView>
-    );
+                showsUserLocation={true}
+                showsCompass={true}
+                showsScale={true}
+            >
+                {businessData.map((business) => (
+                    <MarkerMaker key={business.name} data={business} />
+                ))}
+            </MapView>
+        );
+    }
 };
 
 export default MapViewer;
--- client/src/components/Map/MarkerMaker.js
+++ client/src/components/Map/MarkerMaker.js
@@ -1,1 +1,19 @@
+import { View } from "react-native";
+import { Marker } from "react-native-maps";
+
 // ?? THis is where the markers for each building within radius can be made and then rendered through in a map or something to the MapViewer main component
+const MarkerMaker = ({ data }) => {
+    console.log(data);
+    const lat = data.latitude;
+    const lon = data.longitude;
+
+    return (
+        <Marker
+            coordinate={{ latitude: lat, longitude: lon }}
+            title={data.name}
+            description={data.location}
+        />
+    );
+};
+
+export default MarkerMaker;
